5cc528fcb4bc1b299a5a9b6fe14008bcbc87e4ba
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-06-09 14:18:56 -0400

    first commit

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import './searchbar.css';
+
+class SearchBar {
+  render () {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <a>SEARCH</a>
+      </div>
+
+
+
+
+    );
+  }
+}
+
+export default SearchBar;
